94755d680e7a020e404d072ad942a2627e8b7648
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-10-22 18:09:20 +0200

    ADD qualche piccola modifica con bootstrap

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const lamp = document.getElementById("lamp");
 const button = document.getElementById("turnOnOff");
-console.log(lamp);
 button.addEventListener("click", function () {
   if (lamp.src.includes("yellow_lamp.png")) {
     lamp.src = "./img/white_lamp.png";
